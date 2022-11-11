ae2ae8f572298be30d9503dd786d2c03884cb07f
Lucas Vieira <vieira.lucasmm@gmail.com>
2022-11-11 16:18:31 -0300

    initial apis

---
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -0,0 +1,33 @@
+using BuberBreakfast.Contracts.Breakfast;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberBreakfast.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BreakfastsController : ControllerBase 
+{
+    [HttpPost()]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request) 
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetBreakfast(Guid id) 
+    {
+        return Ok(id);
+    }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+}
